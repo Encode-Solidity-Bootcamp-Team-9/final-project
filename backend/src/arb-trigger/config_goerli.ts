8f45dd2e178d06954c019ad3a2e1d433cb64031c
ukosic <ukosic@gmail.com>
2023-04-03 23:26:04 +0200

    Config cleanup

--- backend/src/arb-trigger/config_goerli.ts
+++ backend/src/arb-trigger/config_goerli.ts
@@ -1,21 +1,34 @@
 import {SupportedChainId, Token} from '@uniswap/sdk-core'
 import {FeeAmount} from "@uniswap/v3-sdk";
-import {BigNumber} from "ethers";
 
+
+// Enum of Dexes to use in arbitrage
 export enum Dex {
   UNISWAP,
   SUSHISWAP
 }
 
 export const CHAIN_ID = SupportedChainId.GOERLI;
+
+// UNISWAP CONTRACTS
 export const UNISWAP_FACTORY_ADDRESS =
   '0x1F98431c8aD98523631AE4a59f267346ea31F984'
 export const UNISWAP_QUOTER_ADDRESS = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6'
-export const UNISWAP_POOL_FEE_TIER = FeeAmount.HIGH;
+export const UNISWAP_POOL_FEE_TIER = FeeAmount.MEDIUM;
+
+// SUSHISWAP CONTRACTS
 export const SUSHISWAP_FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 export const SUSHISWAP_SWAP_ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
-export const ARBITRAGE_CONTRACT_ADDRESS = '0x180b30Cca80073E5a2807CA3343dB96A2C0A6995';
 
+// ARBITRAGE CONTRACTS
+export const ARBITRAGE_CONTRACT_ADDRESS = '0x180b30Cca80073E5a2807CA3343dB96A2C0A6995';
+export const ARBITRAGE_CONTRACT_ABI = [
+  'function performArbitrage(uint8 _sellAt, address _tokenToSell, address _tokenToBuy, uint256 _amount) external',
+  'function totalProfits() view returns (uint256)',
+  'function totalStaked() view returns (uint256)',
+]
+
+// ARBITRAGE TOKENS
 export const TOKEN_STAKING = new Token(
   CHAIN_ID,
   '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
@@ -30,16 +43,10 @@ export const TOKEN_LOAN = new Token(
   18,
   'WETH',
   'Wrapped Ether'
-)
-
+);
 export const TOKEN_PAIR = [TOKEN_STAKING, TOKEN_LOAN].sort((a, b) => BigInt(a.address) > BigInt(b.address) ? 1 : -1)
 
-export const MAX_GAS_PRICE_IN_ETH = "0.0001";
-
-export const ARBITRAGE_CONTRACT_ABI = [
-  'function performArbitrage(uint8 _sellAt, address _tokenToSell, address _tokenToBuy, uint256 _amount)',
-  'function totalProfits() view returns (uint256)',
-  'function totalStaked() view returns (uint256)',
-]
-
-export const PRICE_DIFF_PERCENTAGE = 0.5;
+// ADDITIONAL CONFIG
+export const MAX_GAS_PRICE_IN_ETH = "0.0001"; //how much gas price we are willing to pay
+export const PRICE_DIFF_PERCENTAGE = 0.5; // = 0.5% how much price difference triggers the arbitrage process
+// (it can still not be executed if additional calculations show that it is not profitable)
--- backend/src/arb-trigger/config_mumbai.ts
+++ backend/src/arb-trigger/config_mumbai.ts
@@ -1,21 +1,34 @@
 import {SupportedChainId, Token} from '@uniswap/sdk-core'
 import {FeeAmount} from "@uniswap/v3-sdk";
-import {BigNumber} from "ethers";
 
+
+// Enum of Dexes to use in arbitrage
 export enum Dex {
   UNISWAP,
   SUSHISWAP
 }
 
 export const CHAIN_ID = SupportedChainId.POLYGON_MUMBAI;
+
+// UNISWAP CONTRACTS
 export const UNISWAP_FACTORY_ADDRESS =
   '0x1F98431c8aD98523631AE4a59f267346ea31F984'
 export const UNISWAP_QUOTER_ADDRESS = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6'
 export const UNISWAP_POOL_FEE_TIER = FeeAmount.MEDIUM;
+
+// SUSHISWAP CONTRACTS
 export const SUSHISWAP_FACTORY_ADDRESS = '0xc35DADB65012eC5796536bD9864eD8773aBc74C4'
 export const SUSHISWAP_SWAP_ROUTER_ADDRESS = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506'
-export const ARBITRAGE_CONTRACT_ADDRESS = '0x180b30Cca80073E5a2807CA3343dB96A2C0A6995';
 
+// ARBITRAGE CONTRACTS
+export const ARBITRAGE_CONTRACT_ADDRESS = '0x180b30Cca80073E5a2807CA3343dB96A2C0A6995';
+export const ARBITRAGE_CONTRACT_ABI = [
+  'function performArbitrage(uint8 _sellAt, address _tokenToSell, address _tokenToBuy, uint256 _amount) external',
+  'function totalProfits() view returns (uint256)',
+  'function totalStaked() view returns (uint256)',
+]
+
+// ARBITRAGE TOKENS
 export const TOKEN_STAKING = new Token(
   CHAIN_ID,
   '0x9622F58d9745bAfaeABB7712a69DcdBdcF72e188',
@@ -30,15 +43,9 @@ export const TOKEN_LOAN = new Token(
   'FETH',
   'Fake ETH'
 );
-
 export const TOKEN_PAIR = [TOKEN_STAKING, TOKEN_LOAN].sort((a, b) => BigInt(a.address) > BigInt(b.address) ? 1 : -1)
 
-export const MAX_GAS_PRICE_IN_ETH = "0.0001";
-
-export const ARBITRAGE_CONTRACT_ABI = [
-  'function performArbitrage(uint8 _sellAt, address _tokenToSell, address _tokenToBuy, uint256 _amount)',
-  'function totalProfits() view returns (uint256)',
-  'function totalStaked() view returns (uint256)',
-]
-
-export const PRICE_DIFF_PERCENTAGE = 0.5;
+// ADDITIONAL CONFIG
+export const MAX_GAS_PRICE_IN_ETH = "0.0001"; //how much gas price we are willing to pay
+export const PRICE_DIFF_PERCENTAGE = 0.5; // = 0.5% how much price difference triggers the arbitrage process
+// (it can still not be executed if additional calculations show that it is not profitable)
