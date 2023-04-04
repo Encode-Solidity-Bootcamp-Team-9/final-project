d4ff83ddb3369d6deddabe28003c49860658fd97
ukosic <ukosic@gmail.com>
2023-04-04 20:49:41 +0200

    Added rest endpoint for getting info from chain

---
+++ backend/src/info/info.controller.ts
@@ -0,0 +1,26 @@
+import {Controller, Get, Param} from '@nestjs/common';
+import {UserInfo} from "./dtos/UserInfo";
+import {InfoService} from "./info.service";
+import {Arbitrage} from "./dtos/Arbitrage";
+import {PoolsState} from "./dtos/Pools";
+
+@Controller('info')
+export class InfoController {
+
+  constructor(private readonly infoService: InfoService) {
+  }
+  @Get('user/:address')
+  getUser(@Param('address') address: string): Promise<UserInfo> {
+    return this.infoService.getUser(address);
+  }
+
+  @Get('contract')
+  getArbitrageInfo(): Promise<Arbitrage> {
+    return this.infoService.getArbitrageInfo();
+  }
+
+  @Get('pools')
+  getPoolsInfo(): Promise<PoolsState> {
+    return this.infoService.getPoolsInfo();
+  }
+}
---
+++ backend/src/info/info.service.ts
@@ -0,0 +1,57 @@
+import {Injectable} from '@nestjs/common';
+import {ConfigService} from "@nestjs/config";
+import {ChainProviderService} from "../chain-provider/chain-provider.service";
+import {ContractsProviderService} from "../contracts-provider/contracts-provider.service";
+import {PriceCalculation} from "../arb-trigger/price-calculation-provider.service";
+import {ChooseStrategy} from "../arb-trigger/choose-strategy-provider.service";
+import {DetermineProfitability} from "../arb-trigger/profitability-provider.service";
+import {StrategyExecution} from "../arb-trigger/execution-provider.service";
+import {Results} from "../arb-trigger/results-provider.service";
+import {UserInfo} from "./dtos/UserInfo";
+import {Arbitrage} from "./dtos/Arbitrage";
+import {PoolsState} from "./dtos/Pools";
+
+@Injectable()
+export class InfoService {
+
+  constructor(
+    private readonly contracts: ContractsProviderService,
+    private readonly priceCalculation: PriceCalculation
+  ) {
+  }
+
+  async getUser(address: string): Promise<UserInfo> {
+    const stakeInfo = await this.contracts.arbitrageContract.stakeInfos(address);
+    return {
+      staked: stakeInfo.amount.toString(),
+      totalProfits: stakeInfo.profit.toString(),
+      feth: (await this.contracts.loanToken.balanceOf(address)).toString(),
+      nas: (await this.contracts.stakeToken.balanceOf(address)).toString(),
+      locktime: new Date(stakeInfo.endTS * 1000),
+    };
+  }
+
+  async getArbitrageInfo(): Promise<Arbitrage> {
+    return {
+      address: this.contracts.arbitrageContract.address,
+      totalStaked: await this.contracts.arbitrageContract.totalStaked().toString(),
+      totalProfits: await this.contracts.arbitrageContract.totalProfits().toString()
+    };
+  }
+
+  async getPoolsInfo(): Promise<PoolsState> {
+    const uniPrice = await this.priceCalculation.getUniPoolRatio();
+    const sushiPrice = await this.priceCalculation.getSushiPoolRatio();
+
+    return {
+      nasAddress: this.contracts.stakeToken.address,
+      fethAddress: this.contracts.loanToken.address,
+      uniPoolAddress: this.contracts.uniPool.address,
+      sushiPoolAddress: (await this.contracts.getSushiPool()).address,
+      uniFETH: uniPrice.liquidityLoanToken.toString(),
+      uniNAS: uniPrice.liquidityStakingToken.toString(),
+      sushiFETH: sushiPrice.liquidityLoanToken.toString(),
+      sushiNAS: sushiPrice.liquidityStakingToken.toString(),
+    };
+  }
+}
