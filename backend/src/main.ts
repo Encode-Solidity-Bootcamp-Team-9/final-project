8246229364a75fa1ae0ec7c088e3efb0ff653e1d
Abdelraouf Hecham <raouf.hec@gmail.com>
2023-04-05 12:57:16 +0200

    fix: added cors policy to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
--- front/src/app/pipes/to-eth.pipe.ts
+++ front/src/app/pipes/to-eth.pipe.ts
@@ -3,15 +3,12 @@ import { BigNumber, ethers } from 'ethers';
 
 @Pipe({
   name: 'toETH',
-  standalone: true
+  standalone: true,
 })
 export class ToETHPipe implements PipeTransform {
   transform(value: BigNumber | string | undefined): string {
     if (value === undefined) {
       return '0';
-    }
-    if (typeof value === 'string') {
-      return value;
     }
     let str = ethers.utils.formatEther(value);
     const representation = str.split('.');
