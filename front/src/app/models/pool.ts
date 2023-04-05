83ead5f5cf1b4c37421d7ce8eea96d2945161fba
Abdelraouf Hecham <raouf.hec@gmail.com>
2023-04-05 10:37:58 +0200

    fix: forgot pool.ts on commit

---
+++ front/src/app/models/pool.ts
@@ -0,0 +1,11 @@
+export interface PoolsState {
+  uniPoolAddress: string;
+  sushiPoolAddress: string;
+  uniFETH: string;
+  uniNAS: string;
+  sushiFETH: string;
+  sushiNAS: string;
+
+  uniRatio?: number;
+  sushiRatio?: number;
+}
