59af4975f74ba7030ebe6be81a3a8f31201f10de
alicecha <110170331+alicecha@users.noreply.github.com>
2023-04-06 19:50:40 +0300

    updated deploy args

--- contracts/scripts/arguments.js
+++ contracts/scripts/arguments.js
@@ -1,9 +1,7 @@
-//to verify contract: npx hardhat verify --network mumbai --constructor-args ./scripts/arguments.js [contract-address]
-
 module.exports = [
-      "0xC97727ba966F6C52580121862dF2771A1Ca0F28a",
-      "10000000000000000000",
-      "0x9622F58d9745bAfaeABB7712a69DcdBdcF72e188",
+      "0xB30b5258299397f355ff58B82eFbaC594Ea0A8cc",
+      "50000000000000000000",
+      "0x27b4E260749DDB780ea2AA0C30608c51ee28F6C4",
       1800,
       "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
       "0xE592427A0AEce92De3Edee1F18E0157C05861564"
